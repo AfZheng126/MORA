f1cf595b1062f1345b06cec3da254de8e9750900
AfZheng126 <97977206+AfZheng126@users.noreply.github.com>
2022-10-04 13:18:20 -0400

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,46 @@
+mkdir binaries
+
+# Installing Pufferfish
+
+echo ""
+echo "Building Pufferfish"
+echo ""
+
+cd binaries
+git clone https://github.com/COMBINE-lab/pufferfish.git
+cd pufferfish
+mkdir build
+cd build
+cmake ../
+make
+cd src
+cp pufferfish ../../
+cd ../../../
+
+# Installing Bowtie2
+echo ""
+echo Building Bowtie2
+echo ""
+
+git clone https://github.com/BenLangmead/bowtie2.git
+cd bowtie2
+make
+cd ../
+
+# Installing Minimap2
+echo ""
+echo Building Minimap2
+echo ""
+
+git clone https://github.com/lh3/minimap2
+cd minimap2
+cd make
+cd ../../
+
+
+#Give executable permission to scripts
+cd binaries
+chmod +x pufferfish/pufferfish
+chmod +x bowtie2/bowtie2
+chmod +x bowtie2/bowtie2-build
+chmod +x minimap2/minimap2
